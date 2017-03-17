4ed866f6ccaa4902b78d8a8fe8023416c8cc5b88
Student <student@ai251lab0217m02.aicala.edmc.stu>
2017-03-17 09:27:23 -0700

    push

---
+++ gulp_boilerplate/src/scripts/app.js
@@ -0,0 +1,5 @@
+/** 
+ * Main application
+ */
+
+console.log( 'in app.js' );
---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+/**
+ * Main application
+ */
+
+console.log( 'in app.js' );
+
+import Modernizr from './modernizr';
+import jQuery from './jquery.js'; //Note forced application to window
+import Bootstrap from './bootstrap';
